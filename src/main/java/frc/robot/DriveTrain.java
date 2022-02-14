15451306f777bff64685ea7ce700af176ef3c6a2
ACP-3481 <70781202+ACP-3481@users.noreply.github.com>
2022-02-14 12:57:57 -0700

    Update DriveTrain.java
    Brought the DriveTrain code up to parity with 2020Robot. Added Drivetrain constructor, added TalonDrive(), added the encoder code in a comment.

--- src/main/java/frc/robot/DriveTrain.java
+++ src/main/java/frc/robot/DriveTrain.java
@@ -1,5 +1,6 @@
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.Joystick;
@@ -25,8 +26,43 @@ public class DriveTrain {
     //double leftEncoder;
     //double rightEncoder;
 
+    public DriveTrain(int motor1, int motor2, int motor3, int motor4, Joystick controller1) {
+        leftLeader = new WPI_TalonSRX(motor1);
+        leftFollower = new WPI_TalonSRX(motor2);
+        rightLeader = new WPI_TalonSRX(motor3);
+        rightFollower = new WPI_TalonSRX(motor4);
+        
+        leftFollower.follow(leftLeader);
+        rightFollower.follow(rightLeader);
 
+        myDrive = new DifferentialDrive(leftLeader, rightLeader);
+    }
 
-    
+    public void TalonDrive() {
 
+        throttleValue = controller.getRawAxis(throttleAxis);
+
+        leftStick = controller.getRawAxis(1) / (2 - throttleValue);
+        rightStick = controller.getRawAxis(5) / (2 - throttleValue);
+    }
+
+    public void TalonDriveNoLimiter() {
+        mydrive.tankDrive(controller.getRawAxis(1) * 10, controller.getRawAxis(5) * 10)
+    }
+/* The encoder code, encoders not present yet
+    public void MagEncoder() {
+        leftLeader.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Absolute);
+        rightLeader.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Absolute);
+
+        this.leftEncoder = leftLeader.getSelectedSensorVelocity(0);
+        this.rightEncoder = rightLeader.getSelectedSensorVelocity(0);
+    }
+*/
+
+/* Puts the encoder values onto the Smartdashboard
+    public void SmartDashboard() {
+        SmartDashboard.putNumber("left", leftEncoder);
+        SmartDashboard.putNumber("right", rightEncoder);
+    }
+*/
 }
