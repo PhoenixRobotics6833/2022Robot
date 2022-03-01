c7d001901155fec915dd4f314f41a5fe1f4f286f
ACP-3481 <70781202+ACP-3481@users.noreply.github.com>
2022-03-01 15:40:32 -0700

    Update Autonomous.java

--- src/main/java/frc/robot/Autonomous.java
+++ src/main/java/frc/robot/Autonomous.java
@@ -77,10 +77,10 @@ public class Autonomous {
         }
     }
 
-    public void auto3() {
+    public void DumpPickUp() {
         if(autoStep == 0) {
-            if(timer.get() < 0.0) {
-                // do something
+            if(timer.get() < 2.0) {
+                useRobot.intakeForward();
             }
             else {
                 autoStep = 1;
@@ -90,8 +90,18 @@ public class Autonomous {
             }
         }
         else if(autoStep == 1) {
-            if(timer.get() < 0.0) {
-                //do something
+            if( timer.get() < 2.0) {
+                useRobot.DriveBack();
+            }            
+            else {
+                timer.reset();
+                timer.start();
+                ahrs.reset();
+            }
+        }
+        else if(autoStep == 2) {
+            if(timer.get() < 5.0) {
+                useRobot.rotateToAngle(180);
             }
             else {
                 autoStep = 2;
@@ -100,10 +110,22 @@ public class Autonomous {
                 ahrs.reset();
             }
         }
+        else if(autoStep == 3) {
+            if(timer.get() < 5.0) {
+                useRobot.driveStraightEncoder(1);
+                useRobot.intakeForward();
+            }
+            else {
+                autoStep = 3;
+                timer.reset();
+                timer.start();
+                ahrs.reset();
+            }
+        }
     }
 
     public void auto4() {
-
+        
     }
 
 }
